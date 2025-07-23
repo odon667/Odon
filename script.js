fa5d3b17be3022c8f9c2c49255abe04073102344
odon667 <odonmukanya73@gmail.com>
2025-07-24 00:23:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function getParam(nom) {
 }
 
 window.onload = function () {
-  // Remplir les champs avec les données
+  
   document.getElementById("nomClient").textContent = getParam("nom");
   document.getElementById("telClient").textContent = getParam("telephone");
   document.getElementById("adresseClient").textContent = getParam("adresse");
 
-  // Préparer le lien WhatsApp
-  const produit = "Produit sélectionné"; // Tu peux aussi passer ce paramètre dans l’URL si tu veux
-  const numeroWhatsApp = "243855059661"; // Remplace par ton numéro CallMeBot (sans +)
-  const apikey = "9397153"; // Remplace par ta clé API CallMeBot
+  
+  const produit = "Produit sélectionné"; 
+  const numeroWhatsApp = "243855059661"; 
+  const apikey = "9397153"; 
 
   const message = `Bonjour, j'ai passé une commande :
 - Nom : ${getParam("nom")}
